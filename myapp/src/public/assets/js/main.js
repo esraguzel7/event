6e8cb6054d49248baa6005fdac0afd353bd465c9
SohaElmnefi <117568661+SohaElmnefi@users.noreply.github.com>
2024-08-10 16:20:18 +0300

    install-react-router-dom
    install-react-router-dom

---
+++ myapp/src/public/assets/js/main.js
@@ -0,0 +1,16 @@
+$(function () {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        items: 1,
+        autoplay:true,
+        autoplayTimeout:2500,
+        autoplayHoverPause:true,
+    
+        responsive: {
+            
+        }
+    })
+});
+
---
+++ myapp/src/src/Components/Header.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+function Header() {
+    return (
+        <React.Fragment>
+            <header class="blog-header py-3">
+                <div class="container">
+                    <div class="row flex-nowrap justify-content-between align-items-center">
+                        <div class="col-4 pt-1">
+                            <a class="text-muted" href="/">How it Work?</a>
+                        </div>
+                        <div class="col-4 text-center">
+                            <img src='./logo.png' class="logo" alt="logo" />
+                        </div>
+                        <div class="col-4 d-flex justify-content-end align-items-center">
+                            <a class="btn btn-sm btn-outline-secondary" href="/">Log in</a>&nbsp;
+                            <a class="btn btn-sm btn-outline-secondary" href="/">Sign up</a>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <div class="container">
+                <div class="nav-scroller py-1 mb-2">
+                    <nav class="nav d-flex justify-content-between">
+                        <a class="p-2 text-muted" href="/">Group 1</a>
+                        <a class="p-2 text-muted" href="/">Group 2</a>
+                        <a class="p-2 text-muted" href="/">Group 3</a>
+                        <a class="p-2 text-muted" href="/">Group 4</a>
+                        <a class="p-2 text-muted" href="/">Group 5</a>
+                        <a class="p-2 text-muted" href="/">Group 6</a>
+                        <a class="p-2 text-muted" href="/">Group 7</a>
+                        <a class="p-2 text-muted" href="/">Group 8</a>
+                        <a class="p-2 text-muted" href="/">Group 9</a>
+                        <a class="p-2 text-muted" href="/">Group 10</a>
+                        <a class="p-2 text-muted" href="/">Group 11</a>
+                        <a class="p-2 text-muted" href="/">Group 12</a>
+                    </nav>
+                </div>
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default Header;
