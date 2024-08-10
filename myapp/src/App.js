2569a9ddbdde9b46c4ba80b15da4f10a1c167027
SohaElmnefi <117568661+SohaElmnefi@users.noreply.github.com>
2024-08-10 14:20:34 +0300

    add slider in index
    add slider in index

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Slider />
+  );
+}
+
+function Slider() {
+  return (
+    <div class="owl-carousel owl-theme">
+      <div class="item">
+        <img src="https://picsum.photos/1110/400" alt='Slider img' />
+      </div>
+      <div class="item">
+        <img src="https://picsum.photos/1110/400" alt='Slider img' />
+      </div>
+      <div class="item">
+        <img src="https://picsum.photos/1110/400" alt='Slider img' />
+      </div>
     </div>
   );
 }
