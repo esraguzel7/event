10341ceeccad98de8b96a405eea6cc68e2509637
Esra Güzel <esraravensburg@gmail.com>
2024-08-18 13:26:38 +0300

    finish login page

--- myapp/src/Pages/login.js
+++ myapp/src/Pages/login.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ShowMessages from "../Components/ShowMessages";
 
 function Login() {
     return (
@@ -13,14 +14,14 @@ function Login() {
                         <h2>Login Your Account</h2>
                         <p>Log in to your account and manage your events or join a new event. Fun is waiting for you...</p>
 
-                        <form>
+                        <form method="post" onSubmit={LoginEvent}>
                             <div class="form-group">
                                 <label for="___login__email">Email address</label>
-                                <input type="email" class="form-control" id="___login__email" placeholder="Enter email" />
+                                <input type="email" name="email" class="form-control" id="___login__email" placeholder="Enter email" />
                             </div>
                             <div class="form-group">
                                 <label for="___login__password">Password</label>
-                                <input type="password" class="form-control" id="___login__password" placeholder="Password" />
+                                <input type="password" name="password" class="form-control" id="___login__password" placeholder="Password" />
                             </div>
                             <button type="submit" class="btn btn-primary">Login</button>
                         </form>
@@ -29,6 +30,48 @@ function Login() {
             </div>
         </React.StrictMode>
     );
+}
+
+async function LoginEvent(e) {
+    e.preventDefault();
+
+    var body = {
+        email: e.target.querySelector('input[name=email]').value,
+        password: e.target.querySelector('input[name=password]').value
+    }
+
+    try {
+        const response = await fetch('http://localhost:5001/login', {
+            headers: {
+                'Content-type': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify(body)
+        });
+
+        const data = await response.json();
+        var result = data;
+
+        let message = null;
+
+        if (result.status === false) {
+            message = ShowMessages.ShowErrorMessage(result.message);
+        } else {
+            message = ShowMessages.ShowSuccessMessage(result.message);
+            localStorage.setItem('token', result.token);
+            setTimeout(() => {
+                window.location = '/';
+            }, 2500);
+        }
+
+        e.target.insertBefore(message, e.target.querySelector('button[type=submit]'));
+        setTimeout(() => {
+            message.remove()
+        }, 2500);
+
+    } catch (error) {
+        console.error('Error:', error);
+    }
 }
 
 export default Login;
